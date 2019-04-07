f5cac1b499ef35783950589f5c585bb988f2afce
Pablo <pabloqb@gmail.com>
2019-04-07 09:43:27 +0200

    [FIX] override send_bus_notification on reservation write

--- migrated_hotel/models/inherited_hotel_reservation.py
+++ migrated_hotel/models/inherited_hotel_reservation.py
@@ -1,7 +1,7 @@
 # Copyright 2019  Pablo Q. Barriuso
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HotelReservation(models.Model):
@@ -10,3 +10,13 @@ class HotelReservation(models.Model):
 
     remote_id = fields.Integer(require=True, copy=False, readonly=True,
             help="ID of the target record in the previous version")
+
+    @api.model
+    def create(self, vals):
+        reservation_id = super().create(vals)
+        return reservation_id
+
+    @api.multi
+    def write(self, vals):
+        ret = super().write(vals)
+        return ret
